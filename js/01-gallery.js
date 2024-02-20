cd862c0a2fc885561fd2cadb6553c7b4216cf63f
Alejandra Madera <139291700+alemadera@users.noreply.github.com>
2024-02-20 13:52:03 -0500

    Update js gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,6 +3,9 @@ import { galleryItems } from './gallery-items.js';
 // Obtiene el elemento de la galería en el DOM
 const galleryContainer = document.querySelector('.gallery');
 
+// Variable para almacenar la instancia de la ventana modal
+let instance;
+
 galleryItems.forEach(item => {
     // Crea un nuevo elemento de imagen
     const img = document.createElement('img');
@@ -15,22 +18,19 @@ galleryItems.forEach(item => {
 
     img.addEventListener('click', () => {
         // Abre la imagen a tamaño completo
-        const instance = basicLightbox.create(`
+        instance = basicLightbox.create(`
             <img src="${item.original}" alt="${item.description}">
         `);
         instance.show();
         // Agrega un evento de escucha para la tecla Escape solo cuando la ventana modal esté abierta
         document.addEventListener('keydown', onKeyPress);
-
-        // Define instance fuera del evento clic de la imagen
-        let instance;
     });
 
     galleryContainer.appendChild(img);
 });
 
 function onKeyPress(event) {
-    // Verifica si la tecla presionada es la tecla Escape)
+    // Verificar si la tecla presionada es la tecla Escape)
     if (event.code === 'Escape') {
         // Cierra la ventana modal si existe una instancia y está abierta
         if (instance && instance.visible()) {
