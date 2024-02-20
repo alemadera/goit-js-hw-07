b1e882fc2a971716708e17b492e18b061191db7b
Alejandra Madera <139291700+alemadera@users.noreply.github.com>
2024-02-20 13:46:31 -0500

    Update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,36 +1,36 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 // Obtiene el elemento de la galería en el DOM
-const gallery = document.querySelector('.gallery');
+const galleryContainer = document.querySelector('.gallery');
 
-// Crea elementos de imagen para cada elemento en galleryItems
 galleryItems.forEach(item => {
-  // Crea un nuevo elemento de imagen
-  const img = document.createElement('img');
-  // Establece el atributo src con la URL de la imagen pequeña
-  img.src = item.preview;
-  // Establece el atributo alt con la descripción de la imagen
-  img.alt = item.description;
-  // Agrega una clase para estilos adicionales si es necesario
-  img.classList.add('gallery__image');
-  // Agrega un listener para el evento clic que abre la imagen a tamaño completo
-  img.addEventListener('click', () => {
-    // Abre la imagen a tamaño completo
-    const instance = basicLightbox.create(`
+    // Crea un nuevo elemento de imagen
+    const img = document.createElement('img');
+    // Establece el atributo src con la URL de la imagen pequeña
+    img.src = item.preview;
+    // Establece el atributo alt con la descripción de la imagen
+    img.alt = item.description;
+    // Agrega una clase para estilos adicionales si es necesario
+    img.classList.add('gallery__image');
+
+    img.addEventListener('click', () => {
+        // Abre la imagen a tamaño completo
+        const instance = basicLightbox.create(`
             <img src="${item.original}" alt="${item.description}">
         `);
         instance.show();
         // Agrega un evento de escucha para la tecla Escape solo cuando la ventana modal esté abierta
         document.addEventListener('keydown', onKeyPress);
-});
 
-galleryContainer.appendChild(img);
+        // Define instance fuera del evento clic de la imagen
+        let instance;
+    });
 
+    galleryContainer.appendChild(img);
 });
 
 function onKeyPress(event) {
-    // Verificar si la tecla presionada es la tecla Escape)
+    // Verifica si la tecla presionada es la tecla Escape)
     if (event.code === 'Escape') {
         // Cierra la ventana modal si existe una instancia y está abierta
         if (instance && instance.visible()) {
@@ -40,4 +40,5 @@ function onKeyPress(event) {
         }
     }
 }
+
 console.log(galleryItems);
